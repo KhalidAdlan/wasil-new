7fd43662f2a6d2682cb8b5e4e6211cec731ec3b5
Khalid Adlan <khalid95adlan@gmail.com>
2020-03-10 16:49:16 +0200

    remove now delivery

--- resources/views/home/customer-info.blade.php
+++ resources/views/home/customer-info.blade.php
@@ -19,7 +19,7 @@
   <input  class="form-control" type="text" name="phone2" placeholder="اكتب رقم هاتفك"/>
 </div>
 
-<div class="form-group" id="div_radio"><label class="control-label requiredField" for="radio">الولاية <span class="asteriskField">*</span></label><div class=""><label class="radio-inline"><label class="radio-inline"><input name="radio" type="radio" value="now">&nbsp;&nbsp;&nbsp;توصيل فوري</label><label class="radio-inline"><input name="radio" type="radio" value="khrt">&nbsp;&nbsp;&nbsp; الخرطوم</label><input name="radio" type="radio" value="bhri">&nbsp;&nbsp;&nbsp; بحري</label><label class="radio-inline"><input name="radio" type="radio" value="omdur">&nbsp;&nbsp;&nbsp; امدرمان</label><label class="radio-inline"><input name="radio" type="radio" value="other">&nbsp;&nbsp;&nbsp; ولايات</label></div></div>
+<div class="form-group" id="div_radio"><label class="control-label requiredField" for="radio">الولاية <span class="asteriskField">*</span></label><div class=""><label class="radio-inline"><input name="radio" type="radio" value="khrt">&nbsp;&nbsp;&nbsp; الخرطوم</label><input name="radio" type="radio" value="bhri">&nbsp;&nbsp;&nbsp; بحري</label><label class="radio-inline"><input name="radio" type="radio" value="omdur">&nbsp;&nbsp;&nbsp; امدرمان</label><label class="radio-inline"><input name="radio" type="radio" value="other">&nbsp;&nbsp;&nbsp; ولايات</label></div></div>
 
 <div class="form-group"><label class="control-label requiredField" for="select">المكان <span class="asteriskField">*</span></label><select required class="select form-control" id="select" name="select"><option value="توصيل فوري">توصيل فوري</option></select></div>
 
@@ -44,121 +44,7 @@
 <script>
        var newOptions = {
         
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
-        
-        "توصيل فوري": "توصيل فوري",
+       
          
         };
         
